a0e252414692d2271de365f375eb8c040b86c96b
DESKTOP-8NBDM6F\frank <frankie@prodx.com>
2022-09-02 10:22:09 -0700

    MVP

--- chart-app/src/components/DataTable.js
+++ chart-app/src/components/DataTable.js
@@ -4,7 +4,6 @@ import React, { useState, useEffect } from 'react';
 const DataTable = () => {
     const [dataTableData, setDataTableData] = useState(null);
     const [loaded, setLoaded] = useState(false)
-    // let largest = dataTableData.industries[0].in_occupation_jobs;
     const fetchData = async () => {
         await axios.get('https://run.mocky.io/v3/a2cc3707-8691-4188-8413-6183a7bb3d32')
             .then(res => {
@@ -17,47 +16,34 @@ const DataTable = () => {
     };
 
     useEffect(() => {
-        fetchData()
+        fetchData();
     }, []);
     if (loaded) {
+        let rowCounter = 0;
         return (
             <div>
-                <div className="row">
-                    <div className="region-col column">Industry</div>
-                    <div className="column">Occupation Jobs<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</div>
-                    <div className="column">% of Occupation<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</div>
-                    <div className="column">% of Total<br />Jobs in<br />Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</div>
-                </div>
-                {dataTableData.employing_industries.industries.map((industry) => (
-                            <div className="row">
-                                <div className="region-col column" style={{color: "rgb(0, 150, 255)", backgroundSize: `10%`, backgroundColor:"rgba(135,206,235,0.3)"}}>🏢{industry.title}</div>
-                                <div className="column">{industry.in_occupation_jobs}</div>
-                                <div className="column">{((industry.in_occupation_jobs/dataTableData.employing_industries.jobs)*100).toFixed(1)}</div>
-                                <div className="column">{((industry.in_occupation_jobs/industry.jobs)*100).toFixed(1)}</div>
-                            </div>
-                ))}
+                <h2 className="industry-title">Industries Employing {dataTableData.occupation.title}</h2>
+                <table id="industries">
+                    <tbody>
+                        <tr>
+                            <th className="pick-me"></th>
+                            <th className="region-col">Industry</th>
+                            <th>Occupation Jobs<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
+                            <th>% of Occupation<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
+                            <th>% of Total<br />Jobs in<br />Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
+                        </tr>
+                        {dataTableData.employing_industries.industries.map((industry) => (
+                            <tr key={rowCounter++}>
+                                <td key={rowCounter++} className="bar">{(((industry.in_occupation_jobs/dataTableData.employing_industries.jobs)*100).toFixed(1)) + "%"}</td>
+                                <td key={rowCounter++}>{"🏢 "+(industry.title)}</td>
+                                <td key={rowCounter++}>{industry.in_occupation_jobs}</td>
+                                <td key={rowCounter++}>{((industry.in_occupation_jobs/dataTableData.employing_industries.jobs)*100).toFixed(1)}</td>
+                                <td key={rowCounter++}>{((industry.in_occupation_jobs/industry.jobs)*100).toFixed(1)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
-            // <div>
-            //     <h2>Industries Employing {dataTableData.occupation.title}</h2>
-            //     <table>
-            //         <tbody>
-            //             <tr>
-            //                 <th className="region-col">Industry</th>
-            //                 <th>Occupation Jobs<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
-            //                 <th>% of Occupation<br />in Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
-            //                 <th>% of Total<br />Jobs in<br />Industry<br />&#40;{dataTableData.employing_industries.year}&#41;</th>
-            //             </tr>
-            //             {dataTableData.employing_industries.industries.map((industry) => (
-            //                 <tr>
-            //                     <th style={{color: "rgb(0, 150, 255)", backgroundSize: `10%`, backgroundColor:"rgba(135,206,235,0.3)"}}>🏢{industry.title}</th>
-            //                     <th>{industry.in_occupation_jobs}</th>
-            //                     <th>{((industry.in_occupation_jobs/dataTableData.employing_industries.jobs)*100).toFixed(1)}</th>
-            //                     <th>{((industry.in_occupation_jobs/industry.jobs)*100).toFixed(1)}</th>
-            //                 </tr>
-            //             ))}
-            //         </tbody>
-            //     </table>
-            // </div>
         )
     }
     return <span>Loading...</span>
--- chart-app/src/index.js
+++ chart-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import $ from 'jquery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +9,12 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('DOM Loaded')
+  $("industries tr td:first-child").each(function(){
+          console.log(this)
+          var textval = "<div class='bg' style='width:"+$(this).html()+"'>&nbsp;</div>";        
+          $(this).html(textval);
+      });
+})
